2ad06cc350e3220e7bf736e33485583020cfc4b5
roderic <roderic@gmail.com>
2009-06-02 20:03:11 -0700

    finished the interview, working on the css tabs

--- bt/test.py
+++
@@ -1,8 +0,0 @@
-import spot
-
-a = spot.Spot(30,1000)
-#print a
-print "above: "+ str(a.getAbove()) + " "+ str(a.getAbove().sumDigits())
-print "below: "+ str(a.getBelow()) +" " +  str(a.getBelow().sumDigits())
-print "left: " + str(a.getLeft()) + " " + str(a.getLeft().sumDigits())
-print "right: "+ str(a.getRight()) + " " + str(a.getRight().sumDigits())
