60d75dd5242accda6e5ff724b32b77f3099f7dda
roderic <roderic@gmail.com>
2009-06-04 02:32:11 -0700

    project is a bit closer. Submitting

--- bt/tabs/tabbed.js
+++ bt/tabs/tabbed.js
@@ -2,8 +2,6 @@ if (window.parent && window.parent.synchTab)
 	window.parent.synchTab(window.name);
 
 function synchTab(frameName) {
-	//alert("insynchTab");
-	//alert(frameName);
   	var elList;
 	var i;
 
@@ -11,19 +9,10 @@ function synchTab(frameName) {
 
   	//if (frameName == null);
    		 //return;
-	//alert("did Not return");
 	elList = document.getElementsByTagName("a");
 	for(i = 0; i < elList.length; i++){
-		//alert('hi');
-		//alert(elList[i].target);
 		if(elList[i].target == frameName){
-			//alert("target == frameName");
-			//alert(elList[i].target);
-			//alert(frameName);
-			//alert(elList[i].href);
-			//alert(window.frames[frameName].location);
 			if(elList[i].href==window.frames[frameName].location.href){
-				//alert("href something");
 				elList[i].className += " activeTab";
 				elList[i].blur();
 			}
