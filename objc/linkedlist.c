ae2c90d4c593e8c3b5fc264fcd83e852f4f20674
Roderic Campbell <roderic@Roderic-Campbells-MacBook.local>
2010-03-21 18:23:50 -0700

    adding problem 5

--- objc/linkedlist.c
+++ objc/linkedlist.c
@@ -6,7 +6,31 @@ struct node {
    struct node *next;
 };
 
-int getNth(struct node* head, int n){
+int InsertNth(struct node* head, int n, int value){
+  assert(n >=0); 
+  struct node* current = head;
+  int count = 0;
+  while (current != NULL){
+      if (n-1 == count){
+        //malloc
+		struct node* newNode = malloc(sizeof(struct node));
+		newNode->data = value;
+
+        //set new->next to n
+		newNode->next = current->next;
+
+        //change n-1th->next to new
+		current->next = newNode;
+
+		return current->data;
+	  }
+	  count++;
+      current = current->next;
+  }
+  return -1;
+}
+
+int GetNth(struct node* head, int n){
   assert(n >=0); 
   struct node* current = head;
   int count = 0;
@@ -45,6 +69,17 @@ int Length(struct node* head){
   }
   printf("final count %d\n", count);
   return count;
+}
+
+int PrintList(struct node* head){
+  struct node* current = head;
+  printf("entering loop\n");
+  while (current != NULL){
+      printf("%d", current->data);
+      current = current->next;
+  }
+  printf("\n");
+  return 1;
 }
 
 struct node* BuildOneTwoThree(){
@@ -88,6 +123,9 @@ int main(){
   printf("length of 55123 list %d\n", Length(list));
   printf("Problem 1: number of 5's %d\n", Count(list, 5));
   printf("Problem 1: number of 1's %d\n", Count(list, 1));
-  printf("Problem 2: the 3rd (list[2]) element is: %d\n", getNth(list, 2));
+  printf("Problem 2: the 3rd (list[2]) element is: %d\n", GetNth(list, 2));
+  printf("Problem 5: Add 6 as the 3th element");
+  InsertNth(list, 3, 6);
+  PrintList(list);
   return 1;
 }
